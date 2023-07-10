21ac375137ec77be372b4b08e5a843086d521505
Glenn Smith <glenn@vector35.com>
2023-07-09 20:39:36 -0400

    Matrix builds and headless

--- library.cpp
+++ library.cpp
@@ -9,6 +9,12 @@ extern "C"
 
 	BINARYNINJAPLUGIN bool CorePluginInit()
 	{
+		PluginCommand::Register("Test Plugin\\Test", "It's a test action!", [](BinaryView* view) {
+			for (auto& symbol: view->GetSymbols())
+			{
+				LogInfo("%s", symbol->GetFullName().c_str());
+			}
+		});
 		return true;
 	}
 
